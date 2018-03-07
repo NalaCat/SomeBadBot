7a2bc08a2876084d9ec00cf01ee351741f6c211d
NalaCat <nalacyka@gmail.com>
2018-03-07 15:54:36 +0100

    s

---
+++ Ping.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace DbotCore
+{
+    public class ping : ModuleBase<SocketCommandContext>
+    {
+        [Command("xD")]
+        [RequireRole(417246316423282698)]
+
+
+        public async Task PingingTask()
+        {
+            bool prevState = false;
+            while (true)
+            {
+                var client = new TcpClient();
+                try
+                {
+                    client.Connect("89.34.99.82", 30120);
+                }
+                catch
+                {
+                    var embed = new EmbedBuilder { Color = Color.Red };
+                    embed.AddField("Oh no!", "Server is dead!");
+                    await ReplyAsync("", embed: embed);
+                }
+
+                var connected = client.Connected;
+
+                if (connected && !prevState)
+                {
+                    var embed = new EmbedBuilder { Color = Color.Green };
+                    embed.AddField("Hooray", "Server is alive!");
+                    await ReplyAsync("", embed: embed);
+                }
+                if (!connected && prevState)
+                {
+                    var embed = new EmbedBuilder { Color = Color.Red };
+                    embed.AddField("Oh no!", "Server is dead!");
+                    await ReplyAsync("", embed: embed);
+                }
+                prevState = connected;
+                await Task.Delay(10000);
+            }
+        }
+    }
+}
---
+++ RequireRoleAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace DbotCore
+{
+    public class RequireRoleAttribute : PreconditionAttribute
+    {
+        private readonly ulong _requiredRoleId;
+
+        public RequireRoleAttribute(ulong requiredRoleId)
+        {
+            _requiredRoleId = requiredRoleId;
+        }
+        public override Task<PreconditionResult> CheckPermissions(ICommandContext context, CommandInfo command, IServiceProvider services)
+            => Task.FromResult(context.User is IGuildUser user && user.RoleIds.Any(x => x == _requiredRoleId) ? PreconditionResult.FromSuccess() : PreconditionResult.FromError("You don't have permission to do this"));
+    }
+}
+
